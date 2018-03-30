e07ff5351fcc033a8bdfd6c3561669a640e2a7b1
王德勤/80374356 <wdeqin@cmbchina.com>
2018-03-30 09:05:45 +0800

    add fibonacci

--- lang/lambda_test.go
+++ lang/lambda_test.go
@@ -14,3 +14,30 @@ func TestFn(t *testing.T) {
 	t.Log(ff(1))
 	t.Log(ff(2))
 }
+
+func newFib() func() (int, int) {
+	p, q, i := 0, 1, 0
+	return func() (int, int) {
+		p, q = q, p+q
+		i++
+		return i, p
+	}
+}
+
+func fibn(n int) int {
+	p, q := 0, 1
+	for i := 0; i < n; i++ {
+		p, q = q, p+q
+	}
+	return p
+}
+
+func TestFib(t *testing.T) {
+	fib := newFib()
+	for i := 0; i < 10; i++ {
+		k, v := fib()
+		if v != fibn(k) {
+			t.Errorf("mismatch at %d, expected %d, got %d ", k, fibn(k), v)
+		}
+	}
+}
