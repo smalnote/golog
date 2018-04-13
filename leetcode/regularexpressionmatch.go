6e041562e9af7c31cd6e8c50067857b5efb6a4bc
王德勤/80374356 <wdeqin@cmbchina.com>
2018-04-13 15:14:34 +0800

    add empty regularexpressionmatch

---
+++ leetcode/regularexpressionmatch.go
@@ -0,0 +1,20 @@
+package leetcode
+
+// Given an input string (s) and a pattern (p),
+// implement regular expression matching with support
+// for '.' and '*'.
+// '.' Matches any single character.
+// '*' Matches zero or more of the preceding elemnt.
+// The matching should cover the entire input string(not partial).
+// Note: #1 s could be empty and contains only lowercase letters a-z.
+//       #2 p could be empty adn contains only lowercase letters a-z,
+//          and characters like . or *.
+// Examples: ("aa", "a") => false
+//           ("aa", "a*") => true
+//           ("ab", ".*") => true
+//           ("aab", "c*a*b") => true
+//           ("mississippi", "mis*is*p*.") => false
+func isMatch(s, p string) bool {
+	panic("unimplemented")
+	return false
+}
