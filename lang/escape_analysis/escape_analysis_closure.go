68f487107680caee625a39d9132d09f3fa6a0c95
smalnote <smalnote@gmail.com>
2020-11-04 11:00:03 +0800

    add closure escape analysis

---
+++ lang/escape_analysis/escape_analysis_closure.go
@@ -0,0 +1,28 @@
+package main
+
+// 闭包对逃逸分析的影响
+// 闭包, 在匿名函数中引用局部变量
+// go guild -gcflas " -m -m "
+func escapeAnalysisClosure() {
+	counter := createCounter()
+	println(counter())
+	println(counter())
+	createCounterWithoutReturn()
+}
+
+type counter (func() int)
+
+func createCounter() counter {
+	count := 0 // escape to heap
+	return func() int {
+		count++ // reference
+		return count
+	}
+}
+
+func createCounterWithoutReturn() func() {
+	count := 0 // escape
+	return func() {
+		count++ // reference
+	}
+}
