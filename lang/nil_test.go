4f6dcb2f1e5bdec2d5850596bcb2cdf4c7cbb724
smalnote <smalnote@gmail.com>
2020-11-08 01:35:12 +0800

    test range on variable with nil value

--- lang/nil_test.go
+++ lang/nil_test.go
@@ -8,9 +8,15 @@ func TestFormt(t *testing.T) {
 	var p *[10]int
 	t.Logf("%v\n", p)
 	t.Logf("%d\n", len(p))
-	t.Logf("p ==nil ? %t", p == nil)
+	t.Logf("p == nil ? %t", p == nil)
 
 	for i := range p {
+		t.Log(i)
+	}
+
+	var v []int
+	t.Logf("v == nil ? %t ", v == nil)
+	for i := range v {
 		t.Log(i)
 	}
 }
