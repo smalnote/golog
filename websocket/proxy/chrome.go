493a1354f0e3213123b674e2356b70c78dababba
smalnote <smalnote@gmail.com>
2020-12-27 21:49:25 +0800

    optimize chrome startup

--- websocket/proxy/chrome.go
+++ websocket/proxy/chrome.go
@@ -11,6 +11,7 @@ import (
 	"time"
 )
 
+// Chrome represents a headless chrome instance
 type Chrome interface {
 	Start() error
 	GetDebuggerURL() string
@@ -23,10 +24,14 @@ type chromeImpl struct {
 	port                 int
 	WebSocketDebuggerURL string
 	cleanUpErr           error
+	retry                int
 }
 
+// NewChrome create a chrome instance
 func NewChrome() (Chrome, error) {
-	chrome := chromeImpl{}
+	chrome := chromeImpl{
+		retry: 3,
+	}
 	err := chrome.Start()
 	if err != nil {
 		return nil, err
@@ -38,10 +43,12 @@ type version struct {
 	WebSocketDebuggerURL string `json:"webSocketDebuggerUrl"`
 }
 
+// Start run a headless chrome and get its debugger URL
 func (c *chromeImpl) Start() error {
 	if c.started {
 		return nil
 	}
+	start := time.Now()
 	port, err := c.getPort()
 	if err != nil {
 		return err
@@ -56,23 +63,38 @@ func (c *chromeImpl) Start() error {
 	if err := c.cmd.Start(); err != nil {
 		return err
 	}
-	<-time.After(1000 * time.Millisecond)
-	versionURL := fmt.Sprintf("http://127.0.0.1:%d/json/version", port)
-	resp, err := http.Get(versionURL)
-	if err != nil {
-		return err
+	for i := 0; i < (c.retry + 1); i++ {
+		// <-time.After(time.Duration((i+1)*10) * time.Millisecond)
+		url, err := c.getDebuggerURL()
+		c.WebSocketDebuggerURL = url
+		if err == nil {
+			break
+		}
+		if i == c.retry {
+			return fmt.Errorf("get debugger url error: %w", err)
+		}
 	}
-
-	version := version{}
-	err = json.NewDecoder(resp.Body).Decode(&version)
-	if err != nil {
-		return err
-	}
-	c.WebSocketDebuggerURL = version.WebSocketDebuggerURL
-	log.Printf("debugger url: %s", version.WebSocketDebuggerURL)
+	duration := time.Since(start)
+	log.Printf("debugger url: %s, cost: %vms", c.WebSocketDebuggerURL, duration.Milliseconds())
+	c.started = true
 	return nil
 }
 
+func (c *chromeImpl) getDebuggerURL() (string, error) {
+	versionURL := fmt.Sprintf("http://127.0.0.1:%d/json/version", c.port)
+	resp, err := http.Get(versionURL)
+	if err != nil {
+		return "", err
+	}
+	version := version{}
+	err = json.NewDecoder(resp.Body).Decode(&version)
+	if err != nil {
+		return "", err
+	}
+	return version.WebSocketDebuggerURL, nil
+}
+
+// randomly generate a available port of localhost
 func (c *chromeImpl) getPort() (int, error) {
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
@@ -82,10 +104,12 @@ func (c *chromeImpl) getPort() (int, error) {
 	return listener.Addr().(*net.TCPAddr).Port, nil
 }
 
+// GetDebuggerURL return the debugger URL
 func (c *chromeImpl) GetDebuggerURL() string {
 	return c.WebSocketDebuggerURL
 }
 
+// CleanUp kill the chrome process(only execute once)
 func (c *chromeImpl) CleanUp() error {
 	once := sync.Once{}
 	once.Do(c.cleanUpOnce)
