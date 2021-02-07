6d5e4f0fe49452e8df2dcc42211f00ebfe46687e
smalnote <smalnote@gmail.com>
2021-02-07 20:23:08 +0800

    add var chan/map/slice test

---
+++ lang/var_test.go
@@ -0,0 +1,14 @@
+package lang
+
+import "testing"
+
+func TestVarChan(t *testing.T) {
+	var a chan struct{}
+	t.Logf("var a chan struct{} == nil ? %v", a == nil)
+	var b []struct{}
+	t.Logf("var b []struct{} == nil ? %v", b == nil)
+	var c map[string]struct{}
+	t.Logf("var c map[string]struct{} == nil ? %v", c == nil)
+
+	t.Logf("len(c) == %v", len(c))
+}
